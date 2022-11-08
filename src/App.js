3f6c19ff8099eaa60a3a8f3cf452468dfebc3f6f
rlal2022 <lrahul2010@gmail.com>
2022-11-08 02:53:33 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import Form from "./components/Form";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div>
+        <h1>Lambda Eats</h1>
+        <p>You can remove this code and create your own header</p>
+        <Switch>
+          <Route exact path={"/Home"}>
+            <Home component={Home} />
+          </Route>
+          <Route path="/Form/" component={Form} />
+        </Switch>
+      </div>
     </>
   );
 };
